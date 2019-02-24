d7bba79e225c7c18de0d3c6897fde472e747b35a
ZloyLeva <zloyleva@gmail.com>
2019-02-24 18:11:10 +0200

    create users table

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const mysql = require('mysql');
 
 const app = express();
 
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456789root',
+    database : 'blog'
+});
+
+connection.connect();
+
+connection.query('CREATE TABLE IF NOT EXISTS users(' +
+    'id INT NOT NULL AUTO_INCREMENT,' +
+    'PRIMARY KEY(id),' +
+    'name VARCHAR(20),' +
+    'email VARCHAR(30),'+
+    'password VARCHAR(20)'+
+    ')');
+
+
+
 app.listen("3000", err => {
    if(err){
 
