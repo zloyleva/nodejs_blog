8c7c0afe75e155b0c6acf76d40136f9888bea7af
ZloyLeva <zloyleva@gmail.com>
2019-02-24 19:04:39 +0200

    get all users endpoint

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '123456789root',
-    database : 'blog'
+    database : 'blog',
+    multipleStatements: true
 });
 
 connection.connect();
@@ -20,7 +21,29 @@ connection.query('CREATE TABLE IF NOT EXISTS users(' +
     'password VARCHAR(20)'+
     ')');
 
+app.get('/users',(req, res) => {
 
+    console.log(req.query);
+
+    let limitRows = 4;
+    let offset = 0;
+
+    const query1 = `select * from users LIMIT ${limitRows} OFFSET ${offset};`;
+    const query2 = `select count(*) as count from users;`;
+
+
+
+    connection.query(`${query1} ${query2}`, (err, result) => {
+        if(err){}
+        console.log(result);
+        res.send({
+            data: result[0],
+            page: 1,
+            total: result[1][0].count,
+            status: "ok"
+        });
+    })
+});
 
 app.listen("3000", err => {
    if(err){
